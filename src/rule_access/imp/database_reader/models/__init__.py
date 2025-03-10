b279a7c0ab29ed2c9df83583370f3c7bce70db7d
Sergio Ramirez Ajiaco <seramirez@anla.gov.co>
2025-03-10 16:43:11 -0500

    Fix importation models

--- src/rule_access/imp/database_reader/models/__init__.py
+++ src/rule_access/imp/database_reader/models/__init__.py
@@ -2,6 +2,6 @@
 from rule_access.imp.database_reader.models.tables import Table
 from rule_access.imp.database_reader.models.thematic import Thematic
 from rule_access.imp.database_reader.models.relationships import Relationship
+from rule_access.imp.database_reader.models.error_handler_strategy import ErrorHandlerStrategy
 from rule_access.imp.database_reader.models.validation import Validation
 from rule_access.imp.database_reader.models.duplicate_self_table import DuplicateSelfTable
-from rule_access.imp.database_reader.models.error_handler_strategy import ErrorHandlerStrategy
