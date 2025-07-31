7dd55aee5023bbe93d69a5c1e3f00afa3f8b5b3b
David Andres Monroy Camargo <dmonroy@anla.gov.co>
2025-07-31 13:26:16 -0500

    Update requirements and fix return value in taxonomy_revision function

--- src/validators/imp/taxonomy/taxonomy_validator.py
+++ src/validators/imp/taxonomy/taxonomy_validator.py
@@ -376,7 +376,7 @@ def taxonomy_revision(data: pd.DataFrame) -> pd.DataFrame:
     biotico["correl"] = biotico["correl"].apply(lambda x: x if isinstance(x, float) else 0)
     for column in biotico.columns:
         df_genero_limpio[column] = biotico[column]
-    return biotico
+    return df_genero_limpio
 
 
 class TaxonomyValidator(IValidator):
